399fbc5b01c20088c36d246c0e2b966be190b1ae
Baarii <156163352+Baarii@users.noreply.github.com>
2024-02-14 18:53:08 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,5 +2,5 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "YO YO It Be Like That Sometimes");
-
+app.MapGet("/", () => "YO YO It Be Like That Sometimes");
 app.Run();
